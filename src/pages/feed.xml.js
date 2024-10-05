52ea775b4be257b47d8b35b1ec1aeb9379bbbf96
Alfian Oktafireza <alfianokt104@gmail.com>
2024-10-05 09:55:20 +0700

    init landing page

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
-    title: 'Brutal Blog',
-    description: 'Brutal is a theme for Astro',
+    title: 'Belajar Dev',
+    description: 'Platform ini menyediakan akses gratis untuk belajar pemrograman dari para ahli.',
     stylesheet: false,
     site: context.site,
     items: blog.map((post) => ({
